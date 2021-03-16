ca719bc5faf3aaf5763fe8e95f08d0bc64432d80
catel <mdasilvacatel@hotmail.com>
2021-03-15 23:54:48 -0300

    Created login screen

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
+import "assets/css/custom.css";
 
 import Index from "./views/Index.js";
 import Landing from "./views/examples/Landing.js";
--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-// reactstrap components
 import {
   Button,
   Card,
@@ -17,154 +16,117 @@ import {
   Col
 } from "reactstrap";
 
-// core components
-import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
+import CardTitle from "reactstrap/lib/CardTitle";
 
-class Login extends React.Component {
-  componentDidMount() {
+const Login = (props) => {
+  const main = useRef({ scrollTop: 0 });
+
+  useEffect(() => {
     document.documentElement.scrollTop = 0;
     document.scrollingElement.scrollTop = 0;
-    this.refs.main.scrollTop = 0;
-  }
-  render() {
-    return (
-      <>
-        <DemoNavbar />
-        <main ref="main">
-          <section className="section section-shaped section-lg">
-            <div className="shape shape-style-1 bg-gradient-default">
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
-            </div>
-            <Container className="pt-lg-7">
-              <Row className="justify-content-center">
-                <Col lg="5">
-                  <Card className="bg-secondary shadow border-0">
-                    <CardHeader className="bg-white pb-5">
-                      <div className="text-muted text-center mb-3">
-                        <small>Sign in with</small>
-                      </div>
-                      <div className="btn-wrapper text-center">
-                        <Button
-                          className="btn-neutral btn-icon"
-                          color="default"
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <span className="btn-inner--icon mr-1">
-                            <img
-                              alt="..."
-                              src={require("assets/img/icons/common/github.svg")}
-                            />
-                          </span>
-                          <span className="btn-inner--text">Github</span>
-                        </Button>
-                        <Button
-                          className="btn-neutral btn-icon ml-1"
-                          color="default"
-                          href="#pablo"
-                          onClick={e => e.preventDefault()}
-                        >
-                          <span className="btn-inner--icon mr-1">
-                            <img
-                              alt="..."
-                              src={require("assets/img/icons/common/google.svg")}
-                            />
-                          </span>
-                          <span className="btn-inner--text">Google</span>
-                        </Button>
-                      </div>
-                    </CardHeader>
-                    <CardBody className="px-lg-5 py-lg-5">
-                      <div className="text-center text-muted mb-4">
-                        <small>Or sign in with credentials</small>
-                      </div>
-                      <Form role="form">
-                        <FormGroup className="mb-3">
-                          <InputGroup className="input-group-alternative">
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="ni ni-email-83" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input placeholder="Email" type="email" />
-                          </InputGroup>
-                        </FormGroup>
-                        <FormGroup>
-                          <InputGroup className="input-group-alternative">
-                            <InputGroupAddon addonType="prepend">
-                              <InputGroupText>
-                                <i className="ni ni-lock-circle-open" />
-                              </InputGroupText>
-                            </InputGroupAddon>
-                            <Input
-                              placeholder="Password"
-                              type="password"
-                              autoComplete="off"
-                            />
-                          </InputGroup>
-                        </FormGroup>
-                        <div className="custom-control custom-control-alternative custom-checkbox">
-                          <input
-                            className="custom-control-input"
-                            id=" customCheckLogin"
-                            type="checkbox"
+  }, [props])
+
+  return (
+    <>
+      <main ref={main}>
+        <section className="section section-shaped vh-100 login-section">
+          <div className="shape shape-style-1 bg-gradient-default" />
+          <Container className="pt-lg-3">
+            <Row className="justify-content-center">
+              <Col lg="5">
+                <Card className="bg-secondary shadow border-0">
+                  <CardHeader className="bg-secondary">
+                    <CardTitle tag="h3" className="text-center">Login</CardTitle>
+                    <div className="btn-wrapper text-center">
+                      <Button
+                        block
+                        className="btn-neutral btn-icon"
+                        color="default"
+                        href="#"
+                        onClick={e => e.preventDefault()}
+                      >
+                        <span className="btn-inner--icon mr-1">
+                          <img
+                            alt="..."
+                            src={require("assets/img/icons/common/github.svg")}
                           />
-                          <label
-                            className="custom-control-label"
-                            htmlFor=" customCheckLogin"
-                          >
-                            <span>Remember me</span>
-                          </label>
-                        </div>
-                        <div className="text-center">
-                          <Button
-                            className="my-4"
-                            color="primary"
-                            type="button"
-                          >
-                            Sign in
-                          </Button>
-                        </div>
-                      </Form>
-                    </CardBody>
-                  </Card>
-                  <Row className="mt-3">
-                    <Col xs="6">
-                      <a
-                        className="text-light"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <small>Forgot password?</small>
-                      </a>
-                    </Col>
-                    <Col className="text-right" xs="6">
-                      <a
-                        className="text-light"
-                        href="#pablo"
-                        onClick={e => e.preventDefault()}
-                      >
-                        <small>Create new account</small>
-                      </a>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            </Container>
-          </section>
-        </main>
+                        </span>
+                        <span className="btn-inner--text">Sign in with Github</span>
+                      </Button>
+                    </div>
+                  </CardHeader>
+                  <CardBody className="px-lg-5">
+                    <div className="text-center text-muted mb-4">
+                      <small>Or sign in with credentials</small>
+                    </div>
+                    <Form role="form">
+                      <FormGroup className="mb-3">
+                        <InputGroup className="input-group-alternative">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="ni ni-single-02" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input placeholder="Username" type="text" />
+                        </InputGroup>
+                      </FormGroup>
+                      <FormGroup>
+                        <InputGroup className="input-group-alternative">
+                          <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                              <i className="ni ni-lock-circle-open" />
+                            </InputGroupText>
+                          </InputGroupAddon>
+                          <Input
+                            placeholder="Password"
+                            type="password"
+                            autoComplete="off"
+                          />
+                        </InputGroup>
+                      </FormGroup>
+                      <div className="text-center">
+                        <Button
+                          block
+                          className="my-2"
+                          color="primary"
+                          type="button"
+                        >
+                          Sign in
+                        </Button>
+                      </div>
+                      <div className="text-center">
+                        <Button
+                          block
+                          className="my-2"
+                          color="outline-primary"
+                          type="button"
+                        >
+                          Sign Up
+                        </Button>
+                      </div>
+                      <div className="text-center">
+                        <Button
+                          block
+                          className="text-muted"
+                          color="link"
+                          href="#"
+                          onClick={e => e.preventDefault()}
+                        >
+                          <small>Forgot password?</small>
+                        </Button>
+                      </div>
+                    </Form>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </section>
         <SimpleFooter />
-      </>
-    );
-  }
+      </main>
+    </>
+  );
 }
 
 export default Login;
