a31d97f316a2345380b70e1132e72082fa3394aa
catel <mdasilvacatel@hotmail.com>
2021-03-16 23:17:52 -0300

    Removed global css file

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import {
 import './assets/vendor/nucleo/css/nucleo.css';
 import './assets/vendor/font-awesome/css/font-awesome.min.css';
 import './assets/scss/argon-design-system-react.scss';
-import './assets/css/custom.css';
 
 import Index from './views/Index';
 import Landing from './views/examples/Landing';
