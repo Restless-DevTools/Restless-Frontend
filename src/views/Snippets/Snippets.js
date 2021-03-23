8a7f520b230073303124f8bc34be5a7f4c83630d
Francisco Felipe <francisco@attivilog.com.br>
2021-03-22 23:24:34 -0300

    Added the modal component

--- src/views/Snippets/Snippets.js
+++ src/views/Snippets/Snippets.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Card, Col, Container, Row,
+  Card, Col, Container, Row, Button,
 } from 'reactstrap';
 import SnippetForm from './SnippetForm';
+import DefaultModal from '../../components/DefaultModal/DefaultModal';
 import './styles.css';
 
 function Snippets() {
+  const [formModal, setFormModal] = useState(false);
+
+  console.log(formModal);
   return (
     <Container className="pt-8" fluid>
       <Row>
         <Col className="mb-5 mb-xl-0">
-          <Card className="shadow">
+          <Card className="shadow" />
+          <Button onClick={() => setFormModal(!formModal)}>New Snippet</Button>
+          <DefaultModal
+            isOpen={formModal}
+            title="New Snippet"
+            className="snippet-modal"
+            toggleModal={setFormModal}
+          >
             <SnippetForm />
-          </Card>
+          </DefaultModal>
         </Col>
       </Row>
     </Container>
