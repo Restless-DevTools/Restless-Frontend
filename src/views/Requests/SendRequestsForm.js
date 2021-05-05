48aba0d9fc32a4b33eb05cab6a23918111f902a7
Francisco Felipe <francisco@attivilog.com.br>
2021-05-05 18:37:44 -0300

    Added spinner and the id on the relationed objects

--- src/views/Requests/SendRequestsForm.js
+++ src/views/Requests/SendRequestsForm.js
@@ -16,6 +16,7 @@ import {
   Row,
   TabContent,
   TabPane,
+  Spinner,
 } from 'reactstrap';
 import DefaultDynamicForm from '../../components/DefaultDynamicForm/DefaultDynamicForm';
 import DefaultModal from '../../components/DefaultModal/DefaultModal';
@@ -43,6 +44,7 @@ const SendRequestsForm = (props) => {
   const [headerInputs, setHeaderInputs] = useState([{ id: 0, name: '', value: '' }]);
   const [queryInputs, setQueryInputs] = useState([{ id: 0, name: '', value: '' }]);
   const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const [methods] = useState([
     { label: 'GET', value: 'GET' },
@@ -77,6 +79,7 @@ const SendRequestsForm = (props) => {
         return {
           name: element.name,
           value: element.value,
+          requestId,
         };
       }
 
@@ -88,6 +91,7 @@ const SendRequestsForm = (props) => {
 
   const sendRequest = async (sendObject) => {
     try {
+      console.log(sendObject);
       const { data } = await requests.sendRequest(requestId, sendObject);
 
       if (!data.id) {
@@ -101,18 +105,23 @@ const SendRequestsForm = (props) => {
   };
 
   const handleSendRequest = async () => {
+    setLoading(true);
+
     if (!format) {
       openErrorNotification('Format not selected', 'Format');
+      setLoading(false);
       return;
     }
 
     if (!method) {
       openErrorNotification('Method not selected', 'Method');
+      setLoading(false);
       return;
     }
 
     if (!link) {
       openErrorNotification('Link not selected', 'Link');
+      setLoading(false);
       return;
     }
 
@@ -137,6 +146,7 @@ const SendRequestsForm = (props) => {
     } else {
       openErrorNotification(requestInfo.message, 'Request');
     }
+    setLoading(false);
   };
 
   return (
@@ -174,11 +184,21 @@ const SendRequestsForm = (props) => {
                 </FormGroup>
               </Col>
               <Col md="2" className="text-center px-md-0">
-                <Button onClick={handleSendRequest} className="btn-icon" color="primary" type="button">
-                  <span>
-                    <i className="fa fa-send" />
-                  </span>
-                  <span className="btn-inner--text">Send</span>
+                <Button
+                  onClick={handleSendRequest}
+                  className="btn-icon"
+                  color="primary"
+                  type="button"
+                  disabled={loading}
+                >
+                  { loading ? (<Spinner size="sm" />) : (
+                    <>
+                      <span>
+                        <i className="fa fa-send" />
+                      </span>
+                      <span className="btn-inner--text">Send</span>
+                    </>
+                  ) }
                 </Button>
               </Col>
             </Row>
