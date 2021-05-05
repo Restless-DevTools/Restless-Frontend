f245e54e430e836e8c191d8d786d6f4f001cdd6a
catel <mdasilvacatel@hotmail.com>
2021-05-05 02:42:18 -0300

    Added send request and filling of response modal

--- src/views/Requests/Response.js
+++ src/views/Requests/Response.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import Editor from '@monaco-editor/react';
 import React, { useState } from 'react';
 import {
@@ -6,9 +5,15 @@ import {
   Col, Row, Table,
 } from 'reactstrap';
 import './styles.css';
+import FileSizeUtils from '../../utils/FileSizeUtils';
 
-const Response = () => {
-  const [code, setCode] = useState('{\n"success": true,\n "message": "Informações gravadas! " \n}');
+const Response = (props) => {
+  const {
+    status,
+    data,
+    size,
+  } = props.response;
+
   const [showHeaders, setShowHeaders] = useState(true);
 
   return (
@@ -18,20 +23,30 @@ const Response = () => {
           <Editor
             height="50vh"
             theme="vs-dark"
-            language="json"
-            defaultValue={code}
-            onChange={(value) => setCode(value)}
+            defaultLanguage="json"
+            defaultValue={JSON.stringify(data, null, 2)}
+            options={{
+              autoIndent: 'full',
+              formatOnPaste: true,
+              formatOnType: true,
+              readOnly: true,
+            }}
           />
         </Col>
         <Col md="4" className="mt-3 mt-md-0">
           <Row>
-            <h3 className="text-success">HTTP status: 200</h3>
+            <h3 className="text-success">
+              HTTP status:
+              {' '}
+              {status}
+            </h3>
           </Row>
           <Row>
-            <h4>Time: 323ms</h4>
-          </Row>
-          <Row>
-            <h4>Size: 74B</h4>
+            <h4>
+              Size:
+              {' '}
+              {FileSizeUtils.formatFileSize(size)}
+            </h4>
           </Row>
           <Row>
             <Button onClick={() => { setShowHeaders(!showHeaders); }} className="btn-icon" color="primary" type="button">
--- src/views/Requests/SendRequestsForm.js
+++ src/views/Requests/SendRequestsForm.js
@@ -1,17 +1,23 @@
 import Editor from '@monaco-editor/react';
-import React, { useState, useEffect } from 'react';
+import classnames from 'classnames';
+import React, { useEffect, useState } from 'react';
 import Select from 'react-select';
 import {
   Button,
   Card,
   CardBody,
-
   CardHeader,
   Col,
   FormGroup,
   Input,
+  Nav,
+  NavItem,
+  NavLink,
   Row,
+  TabContent,
+  TabPane,
 } from 'reactstrap';
+import DefaultDynamicForm from '../../components/DefaultDynamicForm/DefaultDynamicForm';
 import DefaultModal from '../../components/DefaultModal/DefaultModal';
 import History from './History';
 import Response from './Response';
@@ -19,10 +25,12 @@ import './styles.css';
 
 const SendRequestsForm = (props) => {
   const {
+    requests,
     requestSelected,
     openErrorNotification,
     openSuccessNotification,
   } = props;
+  const [requestId, setRequestId] = useState(requestSelected.id || null);
   const [method, setMethod] = useState(requestSelected.method || 'GET');
   const [format, setFormat] = useState(requestSelected.format || 'JSON');
   const [code, setCode] = useState(requestSelected.body || '');
@@ -30,6 +38,11 @@ const SendRequestsForm = (props) => {
   const [responseModal, setResponseModal] = useState(false);
   const [historyModal, setHistoryModal] = useState(false);
   const [link, setLink] = useState(requestSelected.link || '');
+  const [showAdvancedOptions, setShowAdvancedOptions] = useState(false);
+  const [activeTab, setActiveTab] = useState(1);
+  const [headerInputs, setHeaderInputs] = useState([{ id: 0, name: '', value: '' }]);
+  const [queryInputs, setQueryInputs] = useState([{ id: 0, name: '', value: '' }]);
+  const [response, setResponse] = useState(null);
 
   const [methods] = useState([
     { label: 'GET', value: 'GET' },
@@ -45,6 +58,7 @@ const SendRequestsForm = (props) => {
 
   useEffect(() => {
     if (requestSelected) {
+      setRequestId(requestSelected.id);
       setMethod(requestSelected.method);
       setFormat(requestSelected.format);
       setCode(requestSelected.body);
@@ -53,7 +67,50 @@ const SendRequestsForm = (props) => {
     }
   }, [requestSelected]);
 
-  const sendRequest = () => {
+  const toggleActiveTab = (tab) => {
+    if (activeTab !== tab) setActiveTab(tab);
+  };
+
+  const mountSendObjectArray = (array) => {
+    const updatedArray = array.map((element) => {
+      if (element.name && element.value) {
+        return {
+          name: element.name,
+          value: element.value,
+        };
+      }
+
+      return null;
+    });
+
+    return updatedArray.filter((element) => !!element);
+  };
+
+  const sendRequest = async (sendObject) => {
+    try {
+      const { data } = await requests.sendRequest(requestId, sendObject);
+
+      if (!data.id) {
+        return { isValid: false, message: data.message };
+      }
+
+      return { isValid: true, data };
+    } catch (error) {
+      return { isValid: false, message: error.response.data.message };
+    }
+  };
+
+  const handleSendRequest = async () => {
+    if (!format) {
+      openErrorNotification('Format not selected', 'Format');
+      return;
+    }
+
+    if (!method) {
+      openErrorNotification('Method not selected', 'Method');
+      return;
+    }
+
     if (!link) {
       openErrorNotification('Link not selected', 'Link');
       return;
@@ -63,26 +120,32 @@ const SendRequestsForm = (props) => {
       method,
       name,
       format,
-      groupId: requestSelected.groupId,
       link,
       requestBody: { body: code },
-      requestHeader: [],
-      requestQuery: [],
+      groupId: requestSelected.groupId,
+      requestHeaders: mountSendObjectArray(headerInputs),
+      requestQueries: mountSendObjectArray(queryInputs),
     };
 
-    console.log(sendObject);
-    // call sendRequest
+    const requestInfo = await sendRequest(sendObject);
 
-    setResponseModal(!responseModal);
+    if (requestInfo.isValid) {
+      openSuccessNotification('Request successfully sended', 'Request');
+
+      setResponse(requestInfo.data);
+      setResponseModal(!responseModal);
+    } else {
+      openErrorNotification(requestInfo.message, 'Request');
+    }
   };
 
   return (
     <>
       <Col className="mb-5 mb-xl-0" xl="8">
         <h2 className="text-secondary">
-          Request -
+          Request
           {' '}
-          {name}
+          {name && `- ${name}`}
         </h2>
         <Card className="shadow">
           <CardHeader className="border-0 bg-dracula-secondary">
@@ -92,7 +155,7 @@ const SendRequestsForm = (props) => {
                   <Select
                     options={methods}
                     onChange={(evt) => setMethod(evt.value)}
-                    placeholder="Select Collection"
+                    placeholder="Select method"
                     value={methods
                       .filter((opt) => opt.value === method)}
                     name="method"
@@ -111,7 +174,7 @@ const SendRequestsForm = (props) => {
                 </FormGroup>
               </Col>
               <Col md="2" className="text-center px-md-0">
-                <Button onClick={() => sendRequest()} className="btn-icon" color="primary" type="button">
+                <Button onClick={handleSendRequest} className="btn-icon" color="primary" type="button">
                   <span>
                     <i className="fa fa-send" />
                   </span>
@@ -122,46 +185,99 @@ const SendRequestsForm = (props) => {
           </CardHeader>
           <CardBody>
             <Row className="align-items-center">
-              <Col xs="3" sm="2" md="3" className="text-left mb-3 mb-md-0">
-                <Button onClick={() => setHistoryModal(!historyModal)} className="btn-icon" color="primary" type="button">
+              <Col xs="3" sm="3" md="3" className="text-left mb-3 mb-md-0">
+                <Button
+                  onClick={() => setHistoryModal(!historyModal)}
+                  className="btn-icon"
+                  color="primary"
+                  type="button"
+                >
                   <span>
                     <i className="fa fa-history" />
                   </span>
                 </Button>
               </Col>
-              <Col xs="9" sm="10" md="4" className="mb-3 mb-md-0">
+              <Col xs="12" sm="6" md="6" className="mb-3 mb-md-0">
                 <FormGroup className="mb-0">
                   <Select
                     options={formats}
                     onChange={(evt) => setFormat(evt.value)}
-                    placeholder="Select Format"
+                    placeholder="Select format"
                     value={formats
                       .filter((opt) => opt.value === format)}
                     name="format"
                   />
                 </FormGroup>
               </Col>
-              <Col className="text-center text-md-left">
-                <Button className="btn" color="primary" type="button">
-                  <span className="btn-inner--text">Headers</span>
-                </Button>
-                <Button className="btn" color="primary" type="button">
-                  <span className="btn-inner--text">Query</span>
+              <Col xs="3" sm="3" md="3" className="text-right mb-3 mb-md-0">
+                <Button
+                  onClick={() => setShowAdvancedOptions(!showAdvancedOptions)}
+                  className="btn"
+                  color="primary"
+                  type="button"
+                >
+                  <span className="btn-inner--text">Advanced</span>
                 </Button>
               </Col>
             </Row>
-            {(format === 'JSON') && (
-            <Row className="mt-3">
-              <Col md="12">
-                <Editor
-                  height="50vh"
-                  theme="vs-dark"
-                  language="json"
-                  defaultValue={code}
-                  onChange={(value) => setCode(value)}
-                />
-              </Col>
-            </Row>
+            {(format === 'JSON' || showAdvancedOptions) && (
+              <Row className="mt-3">
+                <Col md="12" lg={showAdvancedOptions ? 6 : 12}>
+                  <Editor
+                    height="50vh"
+                    theme="vs-dark"
+                    language="json"
+                    defaultValue={code}
+                    onChange={(value) => setCode(value)}
+                  />
+                </Col>
+                {showAdvancedOptions && (
+                  <Col md="12" lg="6" className="pr-2 pl-2">
+                    <Nav tabs className="mt-3 mt-lg-0">
+                      <NavItem>
+                        <NavLink
+                          className={classnames({ active: activeTab === 1 })}
+                          onClick={() => { toggleActiveTab(1); }}
+                        >
+                          Headers
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          className={classnames({ active: activeTab === 2 })}
+                          onClick={() => { toggleActiveTab(2); }}
+                        >
+                          Queries
+                        </NavLink>
+                      </NavItem>
+                    </Nav>
+                    <TabContent activeTab={activeTab}>
+                      <TabPane tabId={1}>
+                        <Row className="mt-3">
+                          <Col md="12">
+                            <DefaultDynamicForm
+                              inputs={headerInputs}
+                              setInputs={setHeaderInputs}
+                              inputIdentifier="Header"
+                            />
+                          </Col>
+                        </Row>
+                      </TabPane>
+                      <TabPane tabId={2}>
+                        <Row className="mt-3">
+                          <Col md="12">
+                            <DefaultDynamicForm
+                              inputs={queryInputs}
+                              setInputs={setQueryInputs}
+                              inputIdentifier="Query"
+                            />
+                          </Col>
+                        </Row>
+                      </TabPane>
+                    </TabContent>
+                  </Col>
+                )}
+              </Row>
             )}
           </CardBody>
         </Card>
@@ -172,7 +288,9 @@ const SendRequestsForm = (props) => {
         className="default-modal"
         toggleModal={setResponseModal}
       >
-        <Response />
+        <Response
+          response={response}
+        />
       </DefaultModal>
       <DefaultModal
         isOpen={historyModal}
