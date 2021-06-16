800207518dacd3d69d24af7b1e69d960a3dd1a85
catel <mdasilvacatel@hotmail.com>
2021-06-16 18:49:41 -0300

    added confirmation modal to delete actions

--- src/components/DefaultCardList/DefaultCardList.jsx
+++ src/components/DefaultCardList/DefaultCardList.jsx
@@ -1,8 +1,9 @@
-import { React } from 'react';
+import { useEffect, React, useState } from 'react';
+
 import {
   Button, Card, Col, Row,
 } from 'reactstrap';
-
+import DefaultConfirmationModal from '../DefaultConfirmationModal/DefaultConfirmationModal';
 import './styles.css';
 
 const DefaultCardList = (props) => {
@@ -10,56 +11,79 @@ const DefaultCardList = (props) => {
     list, edit, remove, handleItemClick,
   } = props;
 
+  const [requestId, setRequestId] = useState(null);
+  const [confirmationModal, setConfirmationModal] = useState(false);
+
   const handleRequestClick = (request) => {
     handleItemClick(request);
   };
 
-  return list && (
-    list.map((request) => (
-      <Card key={request.id} className="default-collapse-list-card">
-        <Row className="align-items-center">
-          <Col md="8">
-            <Button
-              color="link"
-              type="button"
-              onClick={() => handleRequestClick(request)}
-              block
-              className="button-link"
-            >
-              {request.name}
-            </Button>
-          </Col>
-          <Col md="4" className="text-right">
-            {edit && (
-            <Button
-              onClick={() => edit(request.id)}
-              className="btn-icon"
-              color="primary"
-              type="button"
-              size="sm"
-            >
-              <span>
-                <i className="fa fa-edit" />
-              </span>
-            </Button>
-            )}
-            {remove && (
-            <Button
-              className="btn-icon"
-              color="danger"
-              type="button"
-              size="sm"
-              onClick={() => remove(request.id)}
-            >
-              <span>
-                <i className="fa fa-trash" />
-              </span>
-            </Button>
-            )}
-          </Col>
-        </Row>
-      </Card>
-    ))
+  useEffect(() => {
+    if (!confirmationModal) {
+      setRequestId(null);
+    }
+  }, [confirmationModal]);
+
+  return (
+    <>
+      {(list && list.length > 0) && (
+        list.map((request) => (
+          <Card key={request.id} className="default-collapse-list-card">
+            <Row className="align-items-center">
+              <Col md="8">
+                <Button
+                  color="link"
+                  type="button"
+                  onClick={() => handleRequestClick(request)}
+                  block
+                  className="button-link"
+                >
+                  {request.name}
+                </Button>
+              </Col>
+              <Col md="4" className="text-right">
+                {edit && (
+                  <Button
+                    onClick={() => edit(request.id)}
+                    className="btn-icon"
+                    color="primary"
+                    type="button"
+                    size="sm"
+                  >
+                    <span>
+                      <i className="fa fa-edit" />
+                    </span>
+                  </Button>
+                )}
+                {remove && (
+                  <Button
+                    className="btn-icon"
+                    color="danger"
+                    type="button"
+                    size="sm"
+                    onClick={() => {
+                      setConfirmationModal(true);
+                      setRequestId(request.id);
+                    }}
+                  >
+                    <span>
+                      <i className="fa fa-trash" />
+                    </span>
+                  </Button>
+                )}
+              </Col>
+            </Row>
+          </Card>
+        ))
+      )}
+      <DefaultConfirmationModal
+        isOpen={confirmationModal}
+        className="default-small-modal"
+        toggleModal={setConfirmationModal}
+        confirmText="Are you sure you want to delete this request?"
+        confirmAction={() => { remove(requestId); }}
+      />
+    </>
   );
 };
 
--- src/views/Teams/TeamCard.js
+++ src/views/Teams/TeamCard.js
@@ -1,56 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Card, CardBody, CardHeader, Col, Row, Button,
 } from 'reactstrap';
+import DefaultConfirmationModal from '../../components/DefaultConfirmationModal/DefaultConfirmationModal';
 
-function TeamCard(props) {
+const TeamCard = (props) => {
   const { team, editTeam, deleteTeam } = props;
+
+  const [confirmationModal, setConfirmationModal] = useState(false);
+  const [teamToDelete, setTeamToDelete] = useState(null);
 
   const teamsIntegrantsStringfied = team.integrants
     ? team.integrants.map((integrant) => `@${integrant.username}`).join(', ')
     : '';
 
-  return (
-    <Card className="team-card shadow" key={team.id}>
-      <CardHeader>
-        <Row>
-          <Col md="10">
-            Team -
-            {' '}
-            {team.name}
-          </Col>
-          <Col md="2">
-            <Button
-              color="success"
-              type="button"
-              onClick={() => editTeam(team.id)}
-            >
-              <i className="fas fa-edit" />
-            </Button>
-            <Button
-              color="danger"
-              type="button"
-              onClick={() => deleteTeam(team.id)}
-            >
-              <i className="fas fa-trash" />
-            </Button>
-          </Col>
-        </Row>
-      </CardHeader>
-      <CardBody>
-        <p>{team.description}</p>
-        <Row>
-          <Col md="8">
-            Integrants:
-            {' '}
-            {teamsIntegrantsStringfied}
-          </Col>
-          <Col md="4" />
-        </Row>
-      </CardBody>
+  useEffect(() => {
+    if (!confirmationModal) {
+      setTeamToDelete(null);
+    }
+  }, [confirmationModal]);
 
-    </Card>
+  return (
+    <>
+      <Card className="team-card shadow" key={team.id}>
+        <CardHeader>
+          <Row>
+            <Col md="10">
+              Team -
+              {' '}
+              {team.name}
+            </Col>
+            <Col md="2">
+              <Button
+                color="success"
+                type="button"
+                onClick={() => editTeam(team.id)}
+              >
+                <i className="fas fa-edit" />
+              </Button>
+              <Button
+                color="danger"
+                type="button"
+                onClick={() => {
+                  setConfirmationModal(true);
+                  setTeamToDelete(team.id);
+                }}
+              >
+                <i className="fas fa-trash" />
+              </Button>
+            </Col>
+          </Row>
+        </CardHeader>
+        <CardBody>
+          <p>{team.description}</p>
+          <Row>
+            <Col md="8">
+              Integrants:
+              {' '}
+              {teamsIntegrantsStringfied}
+            </Col>
+            <Col md="4" />
+          </Row>
+        </CardBody>
+      </Card>
+      <DefaultConfirmationModal
+        isOpen={confirmationModal}
+        className="default-small-modal"
+        toggleModal={setConfirmationModal}
+        confirmText="Are you sure you want to delete this team?"
+        confirmAction={() => { deleteTeam(teamToDelete); }}
+      />
+    </>
   );
-}
+};
 
 export default TeamCard;
